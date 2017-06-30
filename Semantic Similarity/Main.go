402178e79a035b4d1e3acba83fb28435fa853e5e
Dillon Reedy <DJR0008@auburn.edu>
2017-06-30 07:01:02 -0400

    Adding ability to input questions.

--- Semantic Similarity/Main.go
+++ Semantic Similarity/Main.go
@@ -74,16 +74,11 @@ func createMap(booksSentences []string, keys []string) (map[string]map[string]in
 	
 	var result map[string]map[string]int = make(map[string]map[string]int)
 
-	println(len(keys))
 	for k := range keys {
-		//if (keys[k] != nil) {
 		result[keys[k]] = make(map[string]int)
-		//}
 	}
-	println("All initialized")
 
 	for bs := range booksSentences {
-		println(booksSentences[bs])
 		var sentenceWords []string = strings.Split(booksSentences[bs], " ")
 
 		var count int = len(sentenceWords)
@@ -133,6 +128,41 @@ func getUniqueKeys(sanitizedLexicon string) ([]string) {
 	return removeDuplicates(lexiconsWords)
 }
 
+func squared_sum(givenMap map[string]int) (int) {
+	var sum float64 = 0.0
+	for _, value := range givenMap {
+		sum += math.Pow(float64(value), float64(2))
+	}
+	return int(sum)
+}
+
+func calculate_operand(wordMap map[string]int, synonymMap map[string]int) (float64) {
+	var sum float64 = 0.0
+	for key, value := range wordMap {
+		_, ok := synonymMap[key]
+		if (ok) {
+			sum += (float64(value) * float64(synonymMap[key]))
+		}
+	}
+	return sum
+}
+
+
+func cosine_similarity(word string, synonym string, lexicon map[string]map[string]int) (float64) {
+	_, ok := lexicon[synonym]
+	if (!ok) {
+		return 0
+	}
+
+	var wordMap map[string]int = lexicon[word]
+	var synonymMap map[string]int = lexicon[synonym]
+
+	var operand float64 = calculate_operand(wordMap, synonymMap)
+	var divisor float64 = math.Sqrt(float64(squared_sum(wordMap) * squared_sum(synonymMap))) 
+
+	return (operand / divisor)
+}
+
 
 func main() {
 	var lexicon string
@@ -151,10 +181,53 @@ func main() {
 	var booksSentences []string = splitOutSentences(sanitizedLexicon)
 	var booksKeys []string = getUniqueKeys(sanitizedLexicon)
 
-	var wordMap map[string]map[string]int = createMap(booksSentences, booksKeys)
-	
-	for k := range booksKeys {
-		println(booksKeys[k])
-		wordMap[booksKeys[k]]
+	var lexiconMap map[string]map[string]int = createMap(booksSentences, booksKeys)
+
+	for {
+
+		println("Enter main word (input \"0\" to end the program): ")
+		reader := bufio.NewReader(os.Stdin)
+		word, _ := reader.ReadString('\n')
+		word = strings.TrimSpace(word)
+		word = strings.ToLower(word)
+		//println(word, len(word))
+		if word == "0" {
+			os.Exit(0)
+		}
+
+		_, ok := lexiconMap[word]
+
+		if (ok) {
+			println("Please enter the 4 synonyms: ")
+			var synonym1 string = ""
+			var synonym2 string = ""
+			var synonym3 string = ""
+			var	synonym4 string = ""
+
+			synonym1, _ = reader.ReadString('\n')
+			synonym1 = strings.TrimSpace(synonym1)
+			synonym1 = strings.ToLower(synonym1)
+			
+			synonym2, _ = reader.ReadString('\n')
+			synonym2 = strings.TrimSpace(synonym2)
+			synonym2 = strings.ToLower(synonym2)
+			
+			synonym3, _ = reader.ReadString('\n')
+			synonym3 = strings.TrimSpace(synonym3)
+			synonym3 = strings.ToLower(synonym3)
+			
+			synonym4, _ = reader.ReadString('\n')
+			synonym4 = strings.TrimSpace(synonym4)
+			synonym4 = strings.ToLower(synonym4)
+
+			println("The cosine similarity of ", word, " to the following synonyms are: ")
+			println(synonym1, " = ", strconv.FormatFloat(cosine_similarity(word, synonym1, lexiconMap), 'f', 2, 64))
+			println(synonym2, " = ", strconv.FormatFloat(cosine_similarity(word, synonym2, lexiconMap), 'f', 2, 64))
+			println(synonym3, " = ", strconv.FormatFloat(cosine_similarity(word, synonym3, lexiconMap), 'f', 2, 64))
+			println(synonym4, " = ", strconv.FormatFloat(cosine_similarity(word, synonym4, lexiconMap), 'f', 2, 64))
+		} else {
+			println("The word was not in the lexicon.")
+		}
 	}
+
 }
